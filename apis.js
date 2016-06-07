d41de8cb1bf74d6acb40c080c66898fada7ffdcc
Marcode777 <marcosebello7@gmail.com>
2016-06-07 18:03:07 -0400

    modified apis.js

--- apis.js
+++ apis.js
@@ -6,12 +6,14 @@ $(document).ready(function(){
     type:"GET",
     url: "http://api.wunderground.com/api/3758d4a57136a50e/conditions/q/NY/New_York_City.json",
     success: function(data, textStatus, jqXHR){
-      console.log(data);
+      console.log(data.current_observation);
+      console.log(data.response);
       console.log(textStatus);
       console.log(jqXHR);
-      dis +=  "current temp: " + current_observation.temperature_string + "<p><strong>";
-      dis +=  " current wind speed: " + current_observation.wind_string +"<p><strong>";
-      dis += "current wind direction:" + current_observation.wind_dir_string;
+      dis =  "Current location:" + data.current_observation.display_location.city + "<p><strong>";
+      dis +=  "current temp: " + data.current_observation.temperature_string + "<p><strong>";
+      dis +=  " current wind speed: " + data.current_observation.wind_string +"<p><strong>";
+      dis += "current wind direction:" + data.current_observation.wind_dir_string;
       document.getElementById("here").innerHTML = dis;
     },
     error: function(jqXHR, textStatus, errorThrown){
